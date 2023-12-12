0ba71374ed117aeae7c4d5fd68d10fb908aa759e
JoniMora <yoniiran@gmail.com>
2023-12-12 16:39:16 -0300

    Structure

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://devMora:leg.100462@cluster0.xugxw6p.mongodb.net/vx-back02?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://<user>:<password>@c<cluster>.xugxw6p.mongodb.net/vx-back02?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
