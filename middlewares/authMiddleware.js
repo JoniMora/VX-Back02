1e42922ae1ab7d80627e00aa0bd0653d63a3f2a4
JoniMora <yoniiran@gmail.com>
2023-12-13 16:04:15 -0300

    Middleware for authentication

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,21 @@
 const jwt = require('jsonwebtoken')
 
 exports.authenticateToken = (req, res, next) => {
-  // logica de verificacion del token JWT
+    const token = req.header('Authorization')
+
+    if (!token) {
+      return res.status(401).json({ error: 'Acceso no autorizado. Token no proporcionado.' })
+    }
+  
+    try {
+      const decoded = jwt.verify(token, 'firmaTokenVX')
+  
+      req.user = decoded
+  
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401).json({ error: 'Token no válido.' })
+    }
 }
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
 router.post('/forgot-password', authController.forgotPassword)
+
+router.get('/ruta-protegida', authMiddleware, (req, res) => { //solo accede si el middleware verifica los tokens
+  res.json({ message: '¡Esta es una ruta protegida!' })
+})
 
 module.exports = router
 
