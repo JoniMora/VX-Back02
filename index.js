94e48290b8eb1af2bbd5dc970395e9c66f1c949f
JoniMora <yoniiran@gmail.com>
2024-01-02 10:25:38 -0300

    Cors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 require('./db/mongoose')
 const swaggerUI = require('swagger-ui-express')
 const swaggerSpec = require('./config/swaggerConfig')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use(express.json())
 
